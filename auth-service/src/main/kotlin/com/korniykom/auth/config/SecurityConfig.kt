4080be2f2d928ce15b87dc5a2ff919eb79d54bb3
Max <korniykom@gmail.com>
2026-03-23 23:46:42 +0200

    feat: add security config for /auth/api enpoint

---
+++ auth-service/src/main/kotlin/com/korniykom/auth/config/SecurityConfig.kt
@@ -0,0 +1,26 @@
+package com.korniykom.auth.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    fun filterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
+        return httpSecurity
+            .csrf { it.disable() }
+            .authorizeHttpRequests { auth ->
+                auth
+                    .requestMatchers("/api/auth/**")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated()
+            }
+            .build()
+    }
+}
