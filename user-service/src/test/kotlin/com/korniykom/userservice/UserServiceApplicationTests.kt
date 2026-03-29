b37ea19dd380f6f3eb74477c29961a20ac67de42
Max <korniykom@gmail.com>
2026-03-29 22:38:42 +0300

    feat: create spring boot project for user service

---
+++ user-service/src/test/kotlin/com/korniykom/userservice/UserServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.korniykom.userservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
