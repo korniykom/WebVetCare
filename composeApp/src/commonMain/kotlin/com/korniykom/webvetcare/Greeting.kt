1dbf8c54c278875f3f6aaa023ea0c0f530cfe59c
Max <korniykom@gmail.com>
2026-04-05 23:20:58 +0300

    feat: add colors and fonts to frontent

--- composeApp/src/commonMain/kotlin/com/korniykom/webvetcare/Greeting.kt
+++
@@ -1,7 +0,0 @@
-package com.korniykom.webvetcare
-
-class Greeting {
-    fun greet(): String {
-        return "Hello!"
-    }
-}
---
+++ composeApp/src/commonMain/kotlin/com/korniykom/webvetcare/presentation/theme/Colors.kt
@@ -0,0 +1,11 @@
+package com.korniykom.webvetcare.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkNavy  = Color(0xFF0B1F3A)
+val Teal = Color(0xFF1A7F78)
+val White = Color(0xFFF8FAFC)
+val LightGray = Color(0xFFE8EDF2)
+val Amber = Color(0xFFF5A623)
+val ErrorRed = Color(0xFFE53935)
+val Success = Color(0xFF43A047)
