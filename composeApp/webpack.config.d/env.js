8f3f663d3607cc728353a0dd9441d7a0da3ddf0f
Max <korniykom@gmail.com>
2026-04-11 15:21:56 +0300

    fix: update ingres file 2.0

--- composeApp/webpack.config.d/env.js
+++ composeApp/webpack.config.d/env.js
@@ -2,7 +2,7 @@ config.plugins.push({
     apply: (compiler) => {
         compiler.options.plugins.push(
             new (require("webpack")).DefinePlugin({
-                "process.env.BASE_URL": JSON.stringify(process.env.BASE_URL || "http://localhost:8080/api")
+                "process.env.BASE_URL": JSON.stringify(process.env.BASE_URL || "http://webvetcare:8080/api")
             })
         );
     }
